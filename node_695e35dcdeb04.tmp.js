f254b531922ecf52f6fc0b1328c0c44e799a2219
Wupc <wupckoding@gmail.com>
2023-01-31 00:45:56 -0300

    arquivo nao existe

--- node_695e35dcdeb04.tmp.js
+++
@@ -1,9 +0,0 @@
-let btnOne = document.querySelector(".btn1");
-
-function openCookie(event) {
-  event.preventDefault();
-  document.querySelector(".part1").classList.toggle("hide");
-}
-
-btnOne.addEventListener("click", openCookie());
-console.log(btnOne);
